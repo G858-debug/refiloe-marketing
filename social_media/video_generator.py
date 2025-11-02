e5e935f0bdce2b7190cfceccf1a9c8d967ce9f2b
G858-debug <mkhwalo88@gmail.com>
2025-11-02 15:07:24 +0200

    Remove talking_photo_style from character payload
    Removed 'talking_photo_style' from character payload when using photo avatar.

--- social_media/video_generator.py
+++ social_media/video_generator.py
@@ -951,7 +951,6 @@ class VideoGenerator:
             character_payload = {
                 "type": "talking_photo",
                 "talking_photo_id": avatar_id,
-                "talking_photo_style": "expressive",
             }
         else:
             character_payload = {
